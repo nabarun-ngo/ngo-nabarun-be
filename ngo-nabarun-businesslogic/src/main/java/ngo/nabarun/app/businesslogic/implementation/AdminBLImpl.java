59451eba64eeba801b2a1f1c96b2ba2cf0c5b886
Souvik Sarkar <souviksarrkar362@gamil.com>
2024-12-15 23:54:15 +0530

    feat(cron-service) : Added new cron service 'SYNC_SYSTEMS' This will expected to run everyday to sync between systems like auth0.

--- ngo-nabarun-businesslogic/src/main/java/ngo/nabarun/app/businesslogic/implementation/AdminBLImpl.java
+++ ngo-nabarun-businesslogic/src/main/java/ngo/nabarun/app/businesslogic/implementation/AdminBLImpl.java
@@ -1,37 +1,17 @@
 package ngo.nabarun.app.businesslogic.implementation;
 
-import java.util.AbstractMap;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
 import ngo.nabarun.app.businesslogic.IAdminBL;
-import ngo.nabarun.app.businesslogic.businessobjects.CronServiceDetail;
-import ngo.nabarun.app.businesslogic.businessobjects.DonationDetail;
-import ngo.nabarun.app.businesslogic.businessobjects.DonationDetail.DonationDetailFilter;
+import ngo.nabarun.app.businesslogic.businessobjects.ServiceDetail;
 import ngo.nabarun.app.businesslogic.businessobjects.UserDetail.UserDetailFilter;
-import ngo.nabarun.app.businesslogic.businessobjects.WorkDetail.WorkDetailFilter;
-import ngo.nabarun.app.businesslogic.helper.BusinessConstants;
-import ngo.nabarun.app.businesslogic.helper.BusinessObjectConverter;
-import ngo.nabarun.app.common.enums.DonationStatus;
-import ngo.nabarun.app.common.enums.DonationType;
-import ngo.nabarun.app.common.enums.EmailRecipientType;
-import ngo.nabarun.app.common.util.CommonUtils;
-import ngo.nabarun.app.infra.dto.CorrespondentDTO;
-import ngo.nabarun.app.infra.dto.DonationDTO;
 import ngo.nabarun.app.infra.dto.UserDTO;
-import ngo.nabarun.app.infra.dto.WorkDTO;
 @Service
-@Slf4j
+
 public class AdminBLImpl extends BaseBLImpl implements IAdminBL {
 	
 	@Autowired
@@ -52,31 +32,22 @@ public class AdminBLImpl extends BaseBLImpl implements IAdminBL {
 		return commonDO.generateAPIKey(scopes);
 	}
 
-
-	@Async
 	@Override
-	public void cronTrigger(List<CronServiceDetail> triggerDetail) {
-		for(CronServiceDetail trigger:triggerDetail) {
-			try {
-				adminServices(trigger);
-			}catch (Exception e) {
-				log.error("Error in cron service: ",e);
-			}
-		}	
-	}
-
-	@Override
-	public void adminServices(CronServiceDetail trigger) throws Exception {
+	public void adminServices(ServiceDetail trigger) throws Exception {
 		Map<String, String> parameters=trigger.getParameters();
-		switch (trigger.getTriggerName()) {
+		switch (trigger.getName()) {
+		case SYNC_SYSTEMS:
+			commonDO.syncSystems();
+			break;
 		case CREATE_DONATION:
-			createDonation();
+			List<UserDTO> users = userDO.retrieveAllUsers(null, null, new UserDetailFilter()).getContent();
+			donationDO.createBulkMonthlyDonation(users);
 			break;
 		case DONATION_REMINDER_EMAIL:
-			sendDonationReminderEmail();
+			donationDO.sendDonationReminderEmail();
 			break;
 		case UPDATE_DONATION:
-			updateDonations();
+			donationDO.convertToPendingDonation();
 			break;
 		case SYNC_USERS:
 			boolean syncRole=parameters.get("sync_role") == null ? false : parameters.get("sync_role").equalsIgnoreCase("Y");
@@ -85,96 +56,16 @@ public class AdminBLImpl extends BaseBLImpl implements IAdminBL {
 			userDO.syncUserDetail(syncRole,user_id,user_email);
 			break;
 		case TASK_REMINDER_EMAIL:
-			sendTaskReminderEmail();
+			requestDO.sendTaskReminderEmail();
 			break;
 		default:
-			break;
+			throw new Exception("Invalid Service "+trigger.getName());
 		}
 	}
 
-	private void sendTaskReminderEmail() throws Exception {
-		WorkDetailFilter workFilter= new  WorkDetailFilter();
-		workFilter.setCompleted(false);
-	     Map<UserDTO, List<WorkDTO>> groupedByPendingWithUsers = requestDO.retrieveAllWorkItems(null, null, workFilter).getContent().stream()
-	             .filter(work -> work.getPendingWithUsers() != null) // Handle null pendingWithUsers lists
-	             .flatMap(work -> work.getPendingWithUsers().stream()
-	                 .map(user -> new AbstractMap.SimpleEntry<>(user, work))) // Create pairs of UserDTO and WorkDTO
-	             .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
-
-		for(Entry<UserDTO, List<WorkDTO>> workitem:groupedByPendingWithUsers.entrySet()) {
-			List<Map<String, Object>> task_vars=workitem.getValue().stream().map(m->{
-				try {
-					return m.toMap(businessHelper.getDomainKeyValues());
-				} catch (Exception e) {}
-				return null;
-			}).collect(Collectors.toList());
-			CorrespondentDTO recipient= CorrespondentDTO.builder().emailRecipientType(EmailRecipientType.TO).email(workitem.getKey().getEmail()).name(workitem.getKey().getName()).build();
-			Map<String, Object> user_vars=workitem.getKey().toMap(businessHelper.getDomainKeyValues());
-			commonDO.sendEmail(BusinessConstants.EMAILTEMPLATE__WORKITEM_REMINDER, List.of(recipient),Map.of("workItems",task_vars,"user",user_vars,"currentDate",CommonUtils.formatDateToString(CommonUtils.getSystemDate(), "dd MMM yyyy", "IST")));}
-	}
-
-	private void updateDonations() throws Exception {
-		DonationDetailFilter filter = new DonationDetailFilter();
-		filter.setDonationType(List.of(DonationType.REGULAR));
-		filter.setDonationStatus(List.of(DonationStatus.RAISED));
-		List<DonationDTO> raisedDonations=donationDO.retrieveDonations(null, null, filter).getContent();
-		for(DonationDTO donation:raisedDonations) {
-			DonationDetail updates= new DonationDetail();
-			updates.setDonationStatus(DonationStatus.PENDING);
-			donationDO.updateDonation(donation.getId(), updates, null);
-			Thread.sleep(1000);
-		}
-	}
-
-	private void sendDonationReminderEmail() throws Exception {
-		DonationDetailFilter filter = new DonationDetailFilter();
-		filter.setDonationStatus(List.of(DonationStatus.PENDING));
-		Map<UserDTO, List<DonationDTO>> pendingDonations=donationDO.retrieveDonations(null, null, filter).getContent().stream().filter(f->f.getGuest() == Boolean.FALSE).collect(Collectors.groupingBy(g->g.getDonor()));
-		for(Entry<UserDTO, List<DonationDTO>> donations:pendingDonations.entrySet()) {
-			CorrespondentDTO recipient= CorrespondentDTO.builder().emailRecipientType(EmailRecipientType.TO).email(donations.getKey().getEmail()).name(donations.getKey().getName()).build();
-			List<Map<String, Object>> donation_vars=donations.getValue().stream().map(m->{
-				try {
-					return m.toMap(businessHelper.getDomainKeyValues());
-				} catch (Exception e) {}
-				return null;
-			}).collect(Collectors.toList());
-			Map<String, Object> user_vars=donations.getKey().toMap(businessHelper.getDomainKeyValues());
-			commonDO.sendEmail(BusinessConstants.EMAILTEMPLATE__DONATION_REMINDER, List.of(recipient),Map.of("donations",donation_vars,"user",user_vars));
-		}
-	}
-
-	private void createDonation() throws Exception {
-		List<UserDTO> users = userDO.retrieveAllUsers(null, null, new UserDetailFilter()).getContent();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(CommonUtils.getSystemDate());
-		cal.set(Calendar.DAY_OF_MONTH, 1);
-		Date startDate = cal.getTime();
-		cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
-		Date endDate = cal.getTime();
-
-		for (UserDTO user : users) {
-			if (!CommonUtils.isCurrentMonth(user.getAdditionalDetails().getCreatedOn())
-					&& !donationDO.checkIfDonationRaised(user.getProfileId(), startDate, endDate)) {
-				DonationDetail donationDetail = new DonationDetail();
-				donationDetail.setDonorDetails(BusinessObjectConverter.toUserDetail(user,businessHelper.getDomainKeyValues()));
-				donationDetail.setEndDate(endDate);
-				donationDetail.setIsGuest(false);
-				donationDetail.setStartDate(startDate);
-				donationDetail.setDonationType(DonationType.REGULAR);
-				try {
-					DonationDTO donation = donationDO.raiseDonation(donationDetail);
-					log.info("Automatically raised donation id : " + donation.getId());
-				} catch (Exception e) {
-					log.error("Exception occured during automatic donation creation ", e);
-				}
-				Thread.sleep(2000);
-			}
-		}
-	}
-
+	
 	@Override
 	public Map<String, String> updateApiKey(String id, List<String> scopes, boolean revoke) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
--- ngo-nabarun-businesslogic/src/main/java/ngo/nabarun/app/businesslogic/implementation/CommonBLImpl.java
+++ ngo-nabarun-businesslogic/src/main/java/ngo/nabarun/app/businesslogic/implementation/CommonBLImpl.java
@@ -11,13 +11,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.extern.slf4j.Slf4j;
 import ngo.nabarun.app.businesslogic.ICommonBL;
 import ngo.nabarun.app.businesslogic.businessobjects.AdditionalField;
+import ngo.nabarun.app.businesslogic.businessobjects.ServiceDetail;
 import ngo.nabarun.app.businesslogic.businessobjects.DocumentDetail;
 import ngo.nabarun.app.businesslogic.businessobjects.DocumentDetail.DocumentDetailUpload;
+import ngo.nabarun.app.businesslogic.businessobjects.UserDetail.UserDetailFilter;
 import ngo.nabarun.app.businesslogic.businessobjects.KeyValue;
 import ngo.nabarun.app.businesslogic.businessobjects.Paginate;
 import ngo.nabarun.app.businesslogic.domain.CommonDO;
+import ngo.nabarun.app.businesslogic.exception.BusinessException;
 import ngo.nabarun.app.businesslogic.helper.BusinessDomainHelper;
 import ngo.nabarun.app.businesslogic.helper.BusinessObjectConverter;
 import ngo.nabarun.app.common.annotation.NoLogging;
@@ -27,130 +31,132 @@ import ngo.nabarun.app.common.enums.DonationType;
 import ngo.nabarun.app.common.enums.RefDataType;
 import ngo.nabarun.app.common.enums.RequestType;
 import ngo.nabarun.app.common.util.SecurityUtils.AuthenticatedUser;
+import ngo.nabarun.app.infra.dto.UserDTO;
 
-
+@Slf4j
 @Service
-public class CommonBLImpl implements ICommonBL {
-	
-	
-	
+public class CommonBLImpl extends BaseBLImpl implements ICommonBL {
+
 	@Autowired
 	private BusinessDomainHelper businessHelper;
 
 	@Autowired
 	private CommonDO commonDO;
-	
+
 	@Override
-	public void uploadDocuments(MultipartFile[] files, String docIndexId, DocumentIndexType docIndexType) throws Exception {
+	public void uploadDocuments(MultipartFile[] files, String docIndexId, DocumentIndexType docIndexType)
+			throws Exception {
 		Assert.noNullElements(files, "Files cannot be null !");
-		Assert.notNull(docIndexId,"docIndexId must not be null !");
+		Assert.notNull(docIndexId, "docIndexId must not be null !");
 		Assert.notNull(docIndexType, "docIndexType must not be null !");
-		
+
 		for (MultipartFile file : files) {
-			DocumentDetailUpload doc= new DocumentDetailUpload();
+			DocumentDetailUpload doc = new DocumentDetailUpload();
 			doc.setContent(file.getBytes());
 			doc.setContentType(file.getContentType());
 			doc.setOriginalFileName(file.getOriginalFilename());
 			commonDO.uploadDocument(doc, docIndexId, docIndexType);
 		}
 	}
-	
+
 	@Override
-	public void uploadDocuments(List<DocumentDetailUpload> files,String docIndexId, DocumentIndexType docIndexType) throws Exception {
+	public void uploadDocuments(List<DocumentDetailUpload> files, String docIndexId, DocumentIndexType docIndexType)
+			throws Exception {
 		Assert.noNullElements(files, "Files cannot be null !");
-		Assert.notEmpty(files,"Files cannot be empty !");
-		Assert.notNull(docIndexId,"docIndexId must not be null !");
+		Assert.notEmpty(files, "Files cannot be empty !");
+		Assert.notNull(docIndexId, "docIndexId must not be null !");
 		Assert.notNull(docIndexType, "docIndexType must not be null !");
-		
+
 		for (DocumentDetailUpload file : files) {
 			commonDO.uploadDocument(file, docIndexId, docIndexType);
-		}		
+		}
 	}
 
 	@Override
 	public DocumentDetail getDocument(String docId) throws Exception {
-		Assert.notNull(docId,"docId must not be null !");
-		DocumentDetail documentDetail= new DocumentDetail();
+		Assert.notNull(docId, "docId must not be null !");
+		DocumentDetail documentDetail = new DocumentDetail();
 		documentDetail.setDownloadURL(commonDO.getDocumentUrl(docId).toString());
 		return documentDetail;
 	}
-	
+
 	@Override
 	public boolean deleteDocument(String docId) throws Exception {
-		Assert.notNull(docId,"docId must not be null !");
+		Assert.notNull(docId, "docId must not be null !");
 		return commonDO.deleteDocument(docId);
 	}
 
-	
 	@NoLogging
 	@Override
-	public Map<String,List<KeyValue>> getReferenceData(List<RefDataType> names,Map<String,String> attr) throws Exception {
-		Map<String,List<KeyValue>> obj=new HashMap<>();
-		if(names == null || names.contains(RefDataType.DONATION)) {
-			DonationType type=null;
-			DonationStatus currStatus=null;
-			if(attr != null && attr.containsKey("donationType") && attr.containsKey("currentDonationStatus")) {
-				type=DonationType.valueOf(attr.get("donationType"));
-				currStatus=DonationStatus.valueOf(attr.get("currentDonationStatus"));
+	public Map<String, List<KeyValue>> getReferenceData(List<RefDataType> names, Map<String, String> attr)
+			throws Exception {
+		Map<String, List<KeyValue>> obj = new HashMap<>();
+		if (names == null || names.contains(RefDataType.DONATION)) {
+			DonationType type = null;
+			DonationStatus currStatus = null;
+			if (attr != null && attr.containsKey("donationType") && attr.containsKey("currentDonationStatus")) {
+				type = DonationType.valueOf(attr.get("donationType"));
+				currStatus = DonationStatus.valueOf(attr.get("currentDonationStatus"));
 			}
 			obj.putAll(businessHelper.getDonationRefData(type, currStatus));
 		}
-		
-		if(names == null || names.contains(RefDataType.USER)) {
-			String stateCode=null;
-			String countryCode=null;
-			if(attr != null && attr.containsKey("countryCode")) {
-				countryCode=attr.get("countryCode");
+
+		if (names == null || names.contains(RefDataType.USER)) {
+			String stateCode = null;
+			String countryCode = null;
+			if (attr != null && attr.containsKey("countryCode")) {
+				countryCode = attr.get("countryCode");
 			}
-			if(attr != null && attr.containsKey("countryCode") && attr.containsKey("stateCode")) {
-				stateCode=attr.get("stateCode");
-				countryCode=attr.get("countryCode");
+			if (attr != null && attr.containsKey("countryCode") && attr.containsKey("stateCode")) {
+				stateCode = attr.get("stateCode");
+				countryCode = attr.get("countryCode");
 			}
-			obj.putAll(businessHelper.getUserRefData(countryCode,stateCode));
+			obj.putAll(businessHelper.getUserRefData(countryCode, stateCode));
 		}
-		if(names == null || names.contains(RefDataType.ACCOUNT)) {
+		if (names == null || names.contains(RefDataType.ACCOUNT)) {
 			obj.putAll(businessHelper.getAccountRefData());
 		}
-		if(names == null || names.contains(RefDataType.WORKFLOW)) {
-			RequestType type=null;
-			if(attr != null && attr.containsKey("workflowType")) {
-				type=RequestType.valueOf(attr.get("workflowType"));
+		if (names == null || names.contains(RefDataType.WORKFLOW)) {
+			RequestType type = null;
+			if (attr != null && attr.containsKey("workflowType")) {
+				type = RequestType.valueOf(attr.get("workflowType"));
 			}
 			obj.putAll(businessHelper.getWorkflowRefData(type));
 		}
-		if(names == null || names.contains(RefDataType.ADMIN)) {
+		if (names == null || names.contains(RefDataType.ADMIN)) {
 			obj.putAll(businessHelper.getAdminRefData());
 		}
 		return obj;
 	}
-	
+
 	@NoLogging
 	@Override
-	public Map<String,List<KeyValue>> getReferenceData(List<RefDataType> names) throws Exception {
-		return getReferenceData(names,null);
-	}
-	 
-	@Override
-	public List<AdditionalField> getReferenceFields(String identifier) throws Exception {
-		return businessHelper.findAddtlFieldDTOList(identifier).stream().filter(f->!f.isHidden()).map(BusinessObjectConverter::toAdditionalField).collect(Collectors.toList());
+	public Map<String, List<KeyValue>> getReferenceData(List<RefDataType> names) throws Exception {
+		return getReferenceData(names, null);
 	}
 
 	@Override
-	public Paginate<Map<String,String>> getNotifications(Integer pageIndex, Integer pageSize) {
+	public List<AdditionalField> getReferenceFields(String identifier) throws Exception {
+		return businessHelper.findAddtlFieldDTOList(identifier).stream().filter(f -> !f.isHidden())
+				.map(BusinessObjectConverter::toAdditionalField).collect(Collectors.toList());
+	}
+
+	@Override
+	public Paginate<Map<String, String>> getNotifications(Integer pageIndex, Integer pageSize) {
 		return commonDO.getNotifications(pageIndex, pageSize);
 	}
 
-
 	@Async
 	@Override
-	public void manageNotification(AuthenticatedUser user,String action,Map<String, Object> payload) throws Exception {
-		switch(action.toUpperCase()) {
+	public void manageNotification(AuthenticatedUser user, String action, Map<String, Object> payload)
+			throws Exception {
+		switch (action.toUpperCase()) {
 		case "SAVE_TOKEN_AND_GET_COUNTS":
 			commonDO.saveNotificationToken(user.getUserId(), payload.get("token").toString());
 			commonDO.sendDashboardCounts(user.getUserId());
 			break;
 		case "DELETE_TOKEN":
-			commonDO.removeNotificationToken(user.getUserId(),payload.get("token").toString());
+			commonDO.removeNotificationToken(user.getUserId(), payload.get("token").toString());
 			break;
 		case "UPDATE_NOTIFICATION":
 			commonDO.updateNotification(payload.get("id").toString(), payload);
@@ -160,10 +166,41 @@ public class CommonBLImpl implements ICommonBL {
 
 	@Override
 	public List<DocumentDetail> getDocuments(String id, DocumentIndexType type) {
-		return commonDO.getDocuments(id,type).stream().map(BusinessObjectConverter::toDocumentDetail)
+		return commonDO.getDocuments(id, type).stream().map(BusinessObjectConverter::toDocumentDetail)
 				.collect(Collectors.toList());
 	}
-	
-	
-	
+
+	@Async
+	@Override
+	public void cronTrigger(List<ServiceDetail> triggerDetail) {
+		for (ServiceDetail trigger : triggerDetail) {
+			try {
+				//Map<String, String> parameters = trigger.getParameters();
+				switch (trigger.getName()) {
+				case SYNC_SYSTEMS:
+					commonDO.syncSystems();
+					break;
+				case CREATE_DONATION:
+					List<UserDTO> users = userDO.retrieveAllUsers(null, null, new UserDetailFilter()).getContent();
+					donationDO.createBulkMonthlyDonation(users);
+					break;
+				case DONATION_REMINDER_EMAIL:
+					donationDO.sendDonationReminderEmail();
+					break;
+				case UPDATE_DONATION:
+					donationDO.convertToPendingDonation();
+					break;
+				
+				case TASK_REMINDER_EMAIL:
+					requestDO.sendTaskReminderEmail();
+					break;
+				default:
+					throw new BusinessException("Invalid Service " + trigger.getName());
+				}
+			} catch (Exception e) {
+				log.error("Error in cron service: ", e);
+			}
+		}
+	}
+
 }
